bdf0b4154e4c8d625702fca7cb085024bbacc96c
Matheus Alves Almeida <matheusalvesalmeida0@gmail.com>
2022-09-10 10:09:59 -0300

    algoritmo pronto, mas muito lento. Vendo como otimizar

--- src/genetic/GeneticFunctions.java
+++ src/genetic/GeneticFunctions.java
@@ -14,31 +14,19 @@ public class GeneticFunctions {
     }
 
     // Generate population for ackley function
-    public double[][] generatePopulation() {
-        double[][] population = new double[30][30];
-
-        for (int i = 0; i < 30; i++) {
-            for (int j = 0; j < 30; j++) {
-                population[i][j] = Math.random();
+    public double[][] generatePopulation(int populationSize, int chromosomeSize) {
+        double[][] population = new double[populationSize][chromosomeSize];
+        for (int i = 0; i < populationSize; i++) {
+            for (int j = 0; j < chromosomeSize; j++) {
+                population[i][j] = Math.random() * 30 - 15;
             }
         }
 
         return population;
     }
 
-    // Calculate fitness for ackley function
-    public double[] calculateFitness(double[][] population) {
-        double[] fitness = new double[30];
-
-        for (int i = 0; i < 30; i++) {
-            fitness[i] = this.ackleyFunction(population[i]);
-        }
-
-        return fitness;
-    }
-
     // Calculate ackley function
-    public double ackleyFunction(double[] x) {
+    public double calculateFitness(double[] x) {
         double sum1 = 0;
         double sum2 = 0;
 
@@ -50,5 +38,99 @@ public class GeneticFunctions {
         return -a * Math.exp(-b * Math.sqrt(sum1 / d)) - Math.exp(sum2 / d) + a + Math.exp(1);
     }
 
-    // Select parents for crossover
+    // Sort population by fitness
+    public double[][] sortPopulation(double[][] population) {
+        double[][] sortedPopulation = new double[population.length][population[0].length];
+
+        for (int i = 0; i < population.length; i++) {
+            double[] individual = population[i];
+            double fitness = calculateFitness(individual);
+
+            for (int j = 0; j < population.length; j++) {
+                if (calculateFitness(sortedPopulation[j]) < fitness) {
+                    double[] temp = sortedPopulation[j];
+                    sortedPopulation[j] = individual;
+                    individual = temp;
+                }
+            }
+        }
+
+        return sortedPopulation;
+    }
+
+    // Select two parents by roulette wheel selection
+    public double[][] selectParents(double[][] population) {
+        double[][] parents = new double[2][population[0].length];
+        double totalFitness = 0;
+
+        for (int i = 0; i < population.length; i++) {
+            totalFitness += calculateFitness(population[i]);
+        }
+
+        for (int i = 0; i < 2; i++) {
+            double random = Math.random() * totalFitness;
+            double sum = 0;
+
+            for (int j = 0; j < population.length; j++) {
+                sum += calculateFitness(population[j]);
+                if (sum >= random) {
+                    parents[i] = population[j];
+                    break;
+                }
+            }
+        }
+
+        return parents;
+    }
+
+    // Crossover
+    public double[][] crossover(double[][] parents) {
+        double[][] children = new double[2][parents[0].length];
+        double[][] parentsCopy = new double[2][parents[0].length];
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < parents[0].length; j++) {
+                parentsCopy[i][j] = parents[i][j];
+            }
+        }
+
+        for (int i = 0; i < 2; i++) {
+            double[] parent1 = parentsCopy[0];
+            double[] parent2 = parentsCopy[1];
+            double[] child = new double[parents[0].length];
+
+            for (int j = 0; j < child.length; j++) {
+                if (Math.random() < 0.5) {
+                    child[j] = parent1[j];
+                } else {
+                    child[j] = parent2[j];
+                }
+            }
+
+            children[i] = child;
+        }
+
+        return children;
+    }
+
+    // Mutation
+    public double[][] mutate(double[][] children) {
+        for (int i = 0; i < children.length; i++) {
+            for (int j = 0; j < children[0].length; j++) {
+                if (Math.random() < 0.1) {
+                    children[i][j] = Math.random() * 30 - 15;
+                }
+            }
+        }
+
+        return children;
+    }
+
+    public double[][] replaceWorstIndividuals(double[][] population, double[][] children) {
+        for (int i = 0; i < children.length; i++) {
+            population[population.length - 1 - i] = children[i];
+        }
+
+        return population;
+    }
 }
--- src/genetic/Main.java
+++ src/genetic/Main.java
@@ -2,6 +2,31 @@ package genetic;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        GeneticFunctions geneticFunctions = new GeneticFunctions();
+        double[][] population = geneticFunctions.generatePopulation(200, 30);
+        int i = 1;
+
+        while (geneticFunctions.calculateFitness(population[0]) != 0) {
+            System.out.println("Generation " + i);
+            population = geneticFunctions.sortPopulation(population);
+            // Select parents for crossover
+            double[][] parents = geneticFunctions.selectParents(population);
+            // Crossover
+            double[][] children = geneticFunctions.crossover(parents);
+            // Mutate
+            double[][] mutatedChildren = geneticFunctions.mutate(children);
+            // Replace worst individuals with children
+            population = geneticFunctions.replaceWorstIndividuals(population, mutatedChildren);
+            // Sort population
+            population = geneticFunctions.sortPopulation(population);
+            // Print best individual and fitness
+            System.out.print("Best individual: ");
+            for (int j = 0; j < population[0].length; j++) {
+                System.out.print(population[0][j]);
+            }
+            System.out.println();
+            System.out.println("Fitness: " + geneticFunctions.calculateFitness(population[0]));
+            i++;
+        }
     }
 }
